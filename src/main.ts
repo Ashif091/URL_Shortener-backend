6062f40f7595849c2450c35a53473d3cec1ceaae
Ashif091 <fhyvhh091@gmail.com>
2024-12-24 10:57:49 +0530

    fix:env issue

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import * as cookieParser from 'cookie-parser';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3000', 
+    origin:`${process.env.CLIENT_URL}`, 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     exposedHeaders: ["set-cookie"],
-  };
+  };  
   app.use(cookieParser());
   app.enableCors(corsOptions);
   await app.listen(process.env.PORT || 5000);
